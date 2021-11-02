c770839b776ecdebc501a3f7a2b27dc61a3deab7
Lukas <74401119+Luki248@users.noreply.github.com>
2021-11-02 09:57:09 +0100

    first working Rule 110

--- rule110.py
+++ rule110.py
@@ -2,4 +2,33 @@
 
 # rule110.py
 
+NUMBER_OF_COLUMNS = 80
+NUMBER_OF_ROWS = 100
+
 print("Rule 110")
+
+line = " "*(NUMBER_OF_COLUMNS - 1) + "*"
+
+for i in range(NUMBER_OF_ROWS):
+    print(line)
+    next_line = " "
+    for j in range(1, NUMBER_OF_COLUMNS-1):
+        pattern = line[j-1] + line[j] + line[j+1]
+        if pattern == "   ":
+            next_line += " "
+        elif pattern == "  *":
+            next_line += "*"
+        elif pattern == " * ":
+            next_line += "*"
+        elif pattern == " **":
+            next_line += "*"
+        elif pattern == "*  ":
+            next_line += " "
+        elif pattern == "* *":
+            next_line += "*"
+        elif pattern == "** ":
+            next_line += "*"
+        elif pattern == "***":
+            next_line += " "
+    next_line += " "
+    line = next_line
